6970dcdd3562d4e45cb31eff900c7da25e841ed2
daredakke <77235881+daredakke@users.noreply.github.com>
2024-01-28 12:39:52 +0000

    Enemy station attack pattern

--- scripts/enemy_station.gd
+++ scripts/enemy_station.gd
@@ -1,15 +1,27 @@
 class_name EnemyStation
 extends Enemy
 
+@export var fire_rate: float = 0.25
+@export var reload_delay: float = 4.5
+@export var shots_to_fire: int = 3
 
 var _rotation_speed: float = randf_range(0.3, 0.6)
 var _original_speed: float = speed
 var _reduced_speed: float = speed * 0.25
 var _weight: float = 0.0
+var _shots_fired: int = 0
+var _bullets_per_salvo: int = 4
+var _enemy_bullet_scene: PackedScene = preload("res://scenes/enemies/enemy_bullet.tscn")
+
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var reload_timer: Timer = $ReloadTimer
 
 
 func _ready() -> void:
 	super._ready()
+	
+	fire_rate_timer.wait_time = fire_rate
+	reload_timer.wait_time = reload_delay
 	rotation_degrees = randf_range(0, 360)
 	
 	if randf() > 0.5:
@@ -25,3 +37,22 @@ func _process(delta):
 		speed = lerpf(_original_speed, _reduced_speed, _weight)
 		
 		_weight += 0.002
+
+
+func _on_reload_timer_timeout() -> void:
+	fire_rate_timer.start()
+
+
+func _on_fire_rate_timer_timeout() -> void:
+	if _shots_fired < shots_to_fire:
+		for i in range(_bullets_per_salvo):
+			var bullet_instance := _enemy_bullet_scene.instantiate() as Area2D
+			bullet_instance.position = self.position
+			bullet_instance.direction = self.direction.rotated(self.rotation + deg_to_rad(90) * i)
+			add_sibling(bullet_instance)
+		
+		_shots_fired += 1
+		fire_rate_timer.start()
+	else:
+		_shots_fired = 0
+		reload_timer.start()
